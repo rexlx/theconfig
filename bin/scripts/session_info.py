5bae12c5cc46494487a89bbaa6c651b8f8f0e20b
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2022-03-09 19:07:39 -0600

    Update session_info.py

--- bin/scripts/session_info.py
+++ bin/scripts/session_info.py
@@ -7,7 +7,8 @@ import uptime as up
 def get_info():
     uname = getpass.getuser()
     hostname = socket.gethostname()
-    wd = os.path.dirname(os.path.realpath(__file__))
+    # wd = os.path.dirname(os.path.realpath(__file__))
+    wd = os.getcwd()
     with open('/proc/uptime', 'r') as f:
         elapsed_time = f.readline().split()[0]
     u = up.time_in_seconds(elapsed_time)
